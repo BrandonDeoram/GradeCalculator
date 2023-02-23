a78135c92f3f098bca63861b68feeebfca98e35b
BrandonDeoram <brandondeoram8931@gmail.com>
2023-02-23 13:07:45 -0500

    Selectable table

--- scripts/spreadsheet.js
+++ scripts/spreadsheet.js
@@ -1,1 +1,28 @@
 // YOUR CODE GOES HERE
+function deselectAll() {
+    $("td.selected").removeClass("selected");
+  }
+  
+  function selectRow(rowIndex) {
+    deselectAll();
+    $("#spreadsheet tbody tr:eq(" + rowIndex + ") td").addClass("selected");
+  }
+  
+  function selectColumn(colIndex) {
+    deselectAll();
+    $("#spreadsheet tbody td:nth-child(" + (colIndex + 2) + ")").addClass("selected");
+  }
+  
+  $(document).ready(function() {
+    $(".row-header").click(function() {
+      selectRow($(this).data("index"));
+    });
+  
+    $(".column-header").click(function() {
+      selectColumn($(this).data("index"));
+    });
+  
+    $("#spreadsheet td").click(function() {
+      $(this).toggleClass("selected");
+    });
+  });
